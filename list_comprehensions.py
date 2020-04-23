40796c186a73e3627b8e32756d9128b74d762ef4
BrendanArthurRing <brendanarthurring@gmail.com>
2020-04-23 18:48:49 +0900

    fixed problems

--- list_comprehensions.py
+++ list_comprehensions.py
@@ -1,31 +1,32 @@
 # https://www.hackerrank.com/challenges/list-comprehensions/problem
 
-x = int(raw_input())
-y = int(raw_input())
-n = int(raw_input()) 
-ar = []
-p = 0 
+#x = int(raw_input())
+#y = int(raw_input())
+#n = int(raw_input()) 
+#ar = []
+#p = 0 
 
-for i in range(x + 1):
-    for j in range(y + 1):
-        if i + j != n:
-            ar.append([])
-            ar[p] = [ i , j ]
-            p+=1 
-            print(ar)
+#for i in range(x + 1):
+#    for j in range(y + 1):
+#        if i + j != n:
+#            ar.append([])
+#            ar[p] = [ i , j ]
+#            p+=1 
+#            print(ar)
 
-x = int(raw_input())
-y = int(raw_input())
-n = int(raw_input())
+#x = int(raw_input())
+#y = int(raw_input())
+#n = int(raw_input())
 #print [[i, j] for i in range(x + 1) for j in range(y + 1) if ((i + j) != n )] 
 
+x, y, z, n = 1, 1, 1, 2
 
 print([
     [i, j, k] 
     for i in range(x + 1) 
     for j in range(y + 1) 
     for k in range(z + 1) 
-    if ((i + j + k) != n )
+    if ((i + j + k) != n)
     ])
 
 x, y, z, n = int(input()), int(input()), int(input()), int(input())
