2ad0f7dd4547790aea31e3fdcc34326be0aa4124
BrendanArthurRing <brendanarthurring@gmail.com>
2020-04-23 18:07:16 +0900

    complete

--- list_comprehensions.py
+++ list_comprehensions.py
@@ -17,7 +17,7 @@ for i in range(x + 1):
 x = int(raw_input())
 y = int(raw_input())
 n = int(raw_input())
-print [[i, j] for i in range(x + 1) for j in range(y + 1) if ((i + j) != n )] 
+#print [[i, j] for i in range(x + 1) for j in range(y + 1) if ((i + j) != n )] 
 
 
 print([[i, j, k] for i in range(x + 1) for j in range(y + 1) for k in range(z + 1) if ((i + j + k) != n )])
